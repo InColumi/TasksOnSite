146d930cafe26d3f58ae3426af3194e7090561d2
max Icolumi <max77648@gmail.com>
2020-10-29 13:39:42 +0300

    Create s structure Train with the following fields:   1.Number of the train(ID)   2.Destination point(Location)   3.Time of departure(Time) Than try to find the if it's possible to go to the given destination location and output the time of departure.
    Note: Pointer,dynamic and structures have to be used by default.
    Input:
    First line contains n(0 < n < 1001).
    Then n lines inputed in given format:
      First - train's ID[int type]
      Second - destination address[char*]
      Third - departure time[char*]
    Output:
    Ouput the information in the following format ID_DESTINATION_TIME
    It's guaranty that it will be only one train to the one city.
    If there is no case to go by train output "Impossible".
    
    Samples:
    
    № INPUT OUTPUT
    3
    01 Astana 10:10
    21 Semey 16:10
    301 Novosibirst 21:00
    SEMEY
    21_SEMEY_16:10
    
    1
    01 Astana 10:10
    AstanA
    1_ASTANA_10:10

--- TasksOnSite/Main.cpp
+++ TasksOnSite/Main.cpp
@@ -1,7 +1,99 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string ToUpper(string line)
+{
+	int sizeLine = line.size();
+	int number;
+	for(int i = 0; i < sizeLine; i++)
+	{
+		number = line[i];
+		if(number >= 97)
+		{
+			number -= 32;
+		}
+		line[i] = number;
+	}
+	return line;
+}
+
+bool Compea(string line, string name)
+{
+	int size = line.size();
+	
+	int count = 0;
+	int numberLine;
+	int numberName;
+	for(int i = 0; i < size; i++)
+	{
+		numberLine = line[i];
+		numberName = name[i];
+		if(numberLine == numberName ||
+		   numberLine + 32 == numberName ||
+		   numberLine == numberName + 32)
+		{
+			++count;
+		}
+		else
+		{
+			count = 0;
+			break;
+		}
+		
+	}
+	return (count == size) ? true : false;
+}
 
 int main()
 {
+	int countRows;
+	int sizeLine = 3;
+	cin >> countRows;
+	string** lines = new string*[countRows];
+	for(int i = 0; i < countRows; i++)
+	{
+		lines[i] = new string[sizeLine];
+	}
+	string temp;
 
+	for(int i = 0; i < countRows; i++)
+	{
+		for(int j = 0; j < sizeLine; j++)
+		{
+			cin >> temp;
+			lines[i][j] = temp;
+		}
+	}
+
+	string location;
+	cin >> location;
+	location = ToUpper(location);
+
+	bool isFind = false;
+	for(unsigned int i = 0; i < countRows; i++)
+	{
+		if(Compea(lines[i][1], location))
+		{
+			isFind = true;
+			int index = 0;
+			if(lines[i][0][0] == '0')
+			{
+				index = 1;
+			}
+			for(int j = index; j < lines[i][0].size(); j++)
+			{
+				cout << lines[i][0][j];
+			}
+			cout << '_' << location << '_' << lines[i][2];
+			break;
+		}
+	}
+	if(isFind == false)
+	{
+		cout << "Impossible";
+	}
 	return 0;
 }
